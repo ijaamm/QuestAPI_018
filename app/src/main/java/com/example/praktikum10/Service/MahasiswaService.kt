49df8ac548a210705ce16348ef10803b675a7b95
ijaamm <ijammusyaf@gmail.com>
2024-12-30 00:50:57 +0700

    membuat interface mahasiswa service

---
+++ app/src/main/java/com/example/praktikum10/Service/MahasiswaService.kt
@@ -0,0 +1,35 @@
+package com.example.praktikum10.Service
+
+import com.example.praktikum10.model.Mahasiswa
+import okhttp3.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface MahasiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+
+    @POST("insertmahasiswa.php")
+    suspend fun insertMahasiswa( @Body mahasiswa: Mahasiswa)
+
+    @GET("bacamahasiswa.php")
+    suspend fun getAllMahasiswa(): List<Mahasiswa>
+
+    @GET("baca1mahasiswa.php/{nim}")
+    suspend fun getMahasiswaByNim(@Query("nim") nim: String): Mahasiswa
+
+    @PUT("editmahasiswa.php/{nim}")
+    suspend fun updateMahasiswa(@Query("nim") nim: String, @Body mahasiswa: Mahasiswa)
+
+    @DELETE("deletemahasiswa.php/{nim}")
+    suspend fun deleteMahasiswa(@Query("nim") nim: String)
+
+}
