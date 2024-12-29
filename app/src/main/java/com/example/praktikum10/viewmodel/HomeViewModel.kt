51e793b2b7cbf1a4e0a791dd83016ec08e300f2a
ijaamm <ijammusyaf@gmail.com>
2024-12-30 01:00:10 +0700

    membuat class home view model

---
+++ app/src/main/java/com/example/praktikum10/viewmodel/HomeViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.praktikum10.viewmodel
+
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum10.model.Mahasiswa
+import com.example.praktikum10.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class HomeUiState{
+    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs(){
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteMhs(nim: String){
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException){
+                HomeUiState.Error
+            } catch (e: HttpException){
+                HomeUiState.Error
+            }
+        }
+    }
+}
