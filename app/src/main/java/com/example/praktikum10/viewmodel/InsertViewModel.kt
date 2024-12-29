33b6fb687751e61b76c280326d075ce5f48da037
ijaamm <ijammusyaf@gmail.com>
2024-12-30 00:59:08 +0700

    membuat class insert view model

--- app/src/main/java/com/example/praktikum10/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum10/viewmodel/InsertViewModel.kt
@@ -9,7 +9,24 @@ import com.example.praktikum10.model.Mahasiswa
 import com.example.praktikum10.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
+class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = InsertUiEvent())
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
