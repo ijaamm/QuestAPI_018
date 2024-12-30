63ebd494378a59ce0b36bedd9d1fb4f9537424b8
ijaamm <ijammusyaf@gmail.com>
2024-12-30 09:26:31 +0700

    membuat function mahasiswa app

---
+++ app/src/main/java/com/example/praktikum10/viewmodel/MahasiswaApp.kt
@@ -0,0 +1,29 @@
+package com.example.praktikum10.viewmodel
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.example.praktikum10.navigation.PengelolaHalaman
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MahasiswaApp(){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    ){
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ){
+            PengelolaHalaman()
+        }
+    }
+}
